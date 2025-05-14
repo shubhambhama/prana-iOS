d09903b4c9e975b10461de57bfebc8c34f7d1e0f
Shubham Bhama <shubham.b@redbus.com>
2025-05-14 22:26:00 +0530

    Initial Commit

---
+++ Prana/PranaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PranaApp.swift
+//  Prana
+//
+//  Created by Shubham Bhama on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PranaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
